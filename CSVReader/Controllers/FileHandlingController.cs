dde628aef9f177de3f650613eb68fd8a7995d6c0
shakaib akhtar <shakaib.akhtar007@gmail.com>
2020-02-26 15:41:49 +0500

    Export data in html tags to txt file and download it.

---
+++ CSVReader/Controllers/FileHandlingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSVReader.Controllers
+{
+    public class FileHandlingController : Controller
+    {
+        // GET: FileHandling
+        public ActionResult ExportToTxt()
+        {
+            return View();
+        }
+    }
+}
